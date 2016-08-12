2dae229a3f782a816c6e43bc7fcb5e37c091dcf2
Elizabeth Lefever <email": null>
2016-08-12 14:29:05 -0400

    Congregating

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -30,5 +30,20 @@ get '/questions/:id' do
   @question = Question.find(params[:id])
   @author = User.find(@question.user_id)
   @answers = @question.answers
+  if @question.best_answer_id
+    answers_array = @answers.to_a
+    best_answer = answers_array.select { |a| a.id == @question.best_answer_id }[0]
+    answers_array.delete(best_answer)
+    answers_array.unshift(best_answer)
+    @answers = answers_array
+  end
   erb :'questions/index'
+end
+
+post '/questions/best-answer' do
+  a = Answer.find(params[:answerid])
+  q = a.question
+  q.best_answer_id = params[:answerid]
+  q.save
+  redirect "/questions/" + q.id.to_s
 end
